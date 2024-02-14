0b25f05c533fd07901087913b59896c1130c3c96
Uzziel Palma <uzzielpalma99@gmail.com>
2024-02-14 11:34:18 -0600

    202402141133

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "01219112059bff8004ecf2ec0c83ba30",
-"index.html": "53cac3c91fc92bd37c11396eaa6b6d0c",
-"/": "53cac3c91fc92bd37c11396eaa6b6d0c",
-"main.dart.js": "83f3bc961c304b8b1c033ffb7f0d91f0",
+"index.html": "419a97e794c5fce8a394ae9aa837e3be",
+"/": "419a97e794c5fce8a394ae9aa837e3be",
+"main.dart.js": "efd54a1ad657ee75fbfa810252334471",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "bee16341fc503475ede90c731c34d6ab",
 "icons/Icon-192.png": "09738b0248dd5c2a5fde3ad6b8ed3fca",
